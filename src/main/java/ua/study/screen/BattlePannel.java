4134f963dacebf61c57c08f91ea16f096b237b47
Bogdan <bogdan.martines@mail.ru>
2016-06-12 15:15:53 +0300

    Add KeyAdapter

--- src/main/java/ua/study/screen/BattlePannel.java
+++ src/main/java/ua/study/screen/BattlePannel.java
@@ -2,26 +2,64 @@ package ua.study.screen;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics2D;
 import java.awt.HeadlessException;
-import javax.swing.JFrame;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.JPanel;
-import javax.swing.WindowConstants;
 
 public class BattlePannel extends JPanel {
 
     public static final int WIDTH = 300;
     public static final int HEIGHT = 300;
 
-    private boolean running;
+    private boolean leftDirection = false;
+    private boolean rightDirection = true;
+    private boolean upDirection = false;
+    private boolean downDirection = false;
+    private boolean inGame;
 
     public BattlePannel() throws HeadlessException {
+        addKeyListener(new BattleKeyAdapter());
         setBackground(Color.black);
         setFocusable(true);
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
     }
 
-    private void doDrawing() {
 
+    private class BattleKeyAdapter extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            int key = e.getKeyCode();
+
+            if ((key == KeyEvent.VK_LEFT) && (!rightDirection)) {
+                System.out.println("Direction changed to left");
+                leftDirection = true;
+                upDirection = false;
+                downDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_RIGHT) && (!leftDirection)) {
+                System.out.println("Direction changed to right");
+                rightDirection = true;
+                upDirection = false;
+                downDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_UP) && (!downDirection)) {
+                System.out.println("Direction changed to up");
+                upDirection = true;
+                rightDirection = false;
+                leftDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_DOWN) && (!upDirection)) {
+                System.out.println("Direction changed to down");
+                downDirection = true;
+                rightDirection = false;
+                leftDirection = false;
+            }
+        }
     }
 }
