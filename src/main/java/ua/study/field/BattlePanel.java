e9fa937b2e8753cb72b1025be8ada08e5bd32588
Bogdan <bogdan.martines@mail.ru>
2016-06-22 00:04:14 +0300

    Move code related to loading of levels to new class LevelLoader

--- src/main/java/ua/study/field/BattlePanel.java
+++ src/main/java/ua/study/field/BattlePanel.java
@@ -16,6 +16,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -32,15 +33,12 @@ public class BattlePanel extends JPanel implements ActionListener {
     private static final int DELAY = 25;    // 40 FPS
 
     private static final String LEVEL_1_FILE_NAME = "level1.txt";
-    private final String ROW_SEPARATOR = ":";
-    private final String BARRIER_SEPARATOR = ";";
-    private final String COORDINATE_SEPARATOR = ",";
 
     private Tank tank = new Tank(this);
 
     private Timer timer;
 
-    private List<Barrier> barriers = new ArrayList<>();
+    protected List<Barrier> barriers = new ArrayList<>();
 
     public BattlePanel() throws HeadlessException, IOException {
         addKeyListener(new BattleKeyAdapter());
@@ -52,45 +50,56 @@ public class BattlePanel extends JPanel implements ActionListener {
         timer = new Timer(DELAY, this);
         timer.start();
 
-        loadLevel();
+        barriers = LevelLoader.loadLevel(LEVEL_1_FILE_NAME);
     }
 
-    private void loadLevel() throws IOException {
-        try (
-                InputStream inputStream = getClass().getResourceAsStream(LEVEL_1_FILE_NAME);
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        ) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                System.out.println("line = " + line);
-                if (line.contains(ROW_SEPARATOR)) {
-                    loadLine(line);
-                } else {
-                    System.out.println("Level file has on-error entry!");
+    private static class LevelLoader {
+
+        private final static String ROW_SEPARATOR = ":";
+        private final static String BARRIER_SEPARATOR = ";";
+        private final static String COORDINATE_SEPARATOR = ",";
+
+        public static List<Barrier> loadLevel(String levelFileName) throws IOException {
+            List<Barrier> tempBarriers = new ArrayList<>();
+            try (
+                    InputStream inputStream = BattlePanel.class.getResourceAsStream(levelFileName);
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            ) {
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    System.out.println("line = " + line);
+                    if (line.contains(ROW_SEPARATOR)) {
+                        tempBarriers.addAll(loadLine(line));
+                    } else {
+                        System.out.println("Level file has on-error entry!");
+                    }
                 }
+                System.out.println("Loaded barriers = " + tempBarriers);
             }
-            System.out.println("Barriers = " + barriers);
+            return tempBarriers;
+        }
+
+        private static List<Barrier> loadLine(String line) {
+            String[] typeWithCoordinates = line.split(ROW_SEPARATOR);
+
+            BarrierType barrierType = BarrierType.getBarrierType(typeWithCoordinates[0]);
+            System.out.println("barrierType  = " + barrierType);
+            if (barrierType == null) {
+                System.out.println("Line '" + line + "' does not contain proper barrier type!");
+                return Collections.emptyList();
+            }
+
+            List<Barrier> tempBarriers = new ArrayList<>();
+            for (String coordinate : typeWithCoordinates[1].split(BARRIER_SEPARATOR)) {
+                String[] coordinates = coordinate.split(COORDINATE_SEPARATOR);
+                System.out.println("coordinates = " + Arrays.toString(coordinates));
+                tempBarriers.add(
+                        new Barrier(barrierType, coordinates[0], coordinates[1]));
+            }
+            return tempBarriers;
         }
     }
-
-    private void loadLine(String line) {
-        String[] typeWithCoordinates = line.split(ROW_SEPARATOR);
-        BarrierType barrierType = BarrierType.getBarrierType(typeWithCoordinates[0]);
-        System.out.println("barrierType  = " + barrierType);
-        if (barrierType == null) {
-            System.out.println("Line '" + line + "' does not contain proper barrier type!");
-            return;
-        }
-
-        for (String coordinate : typeWithCoordinates[1].split(BARRIER_SEPARATOR)) {
-            String[] coordinates = coordinate.split(COORDINATE_SEPARATOR);
-            System.out.println("coordinates = " + Arrays.toString(coordinates));
-            barriers.add(
-                    new Barrier(barrierType, coordinates[0], coordinates[1]));
-        }
-    }
-
 
     private class BattleKeyAdapter extends KeyAdapter {
 
